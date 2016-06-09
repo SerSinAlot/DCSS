eb57aea92ffb71c16a915f9439c2c6fc28d56dd0
SerSinAlot <aarni.korhonen@aol.com>
2016-06-09 14:05:20 +0300

    Update yhteys.php
    Added/changed
    Line 11 (!$yhteys)
    sähköposti to email
    ryhmä to luokka

--- Public/yhteys.php
+++ Public/yhteys.php
@@ -5,25 +5,25 @@ $password="Passw0rd";
 $dbname="DCSS";
 
 /*Muodosta yhteys*/
-$yhteys = new mysqli_connect($servername, $username, $password, $dbname);
+$yhteys = mysqli_connect($servername, $username, $password, $dbname);
 
 /*Testaa yhteys*/
-if ($yhteys === false) {
-die("Yhteys epäonnistui: " . mysqli_connect_error());
+if (!$yhteys) {
+	die("Yhteys epäonnistui: " . mysqli_connect_error());
 }
 echo "Yhteys onnistui";
 
 $etunimi=mysqli_real_escape_string($yhteys, $_POST['Etunimi']);
 $sukunimi=mysqli_real_escape_string($yhteys, $_POST['Sukunimi']);
-$ryhmä=mysqli_real_escape_string($yhteys, $_POST['Ryhmä']);
-$sähköposti=mysqli_real_escape_string($yhteys, $_POST['Email']);
+$luokka=mysqli_real_escape_string($yhteys, $_POST['Ryhmä']);
+$email=mysqli_real_escape_string($yhteys, $_POST['Email']);
 $puhelin=mysqli_real_escape_string($yhteys, $_POST['PuhNro']);
 $otsikko=mysqli_real_escape_string($yhteys, $_POST['Otsikko']);
 $viesti=mysqli_real_escape_string($yhteys, $_POST['Viesti']);
 $tag=mysqli_real_escape_string($yhteys, $_POST['Tag']);
 
 $sql="INSERT INTO `Tiketit`(`Etunimi`, `Sukunimi`, `Ryhmä`, `Email`, `PuhNro`, `Otsikko`, `Viesti`, `Tag`)
-VALUES ('$etunimi', '$sukunimi', '$ryhmä', '$sähköposti', '$puhelin', '$otsikko', '$viesti', '$tag')"
+VALUES ('$etunimi', '$sukunimi', '$luokka', '$email', '$puhelin', '$otsikko', '$viesti', '$tag')"
 if(mysqli-query($yhteys, $sql)) {
 	echo "Tiedot tallennettu onnistuneesti.";
 } else {
