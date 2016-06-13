bfcf0a6c6886cba5eda4faca03eb193c3c4caaba
Joni Heikkinen <joniheikkinen@kamk.fi>
2016-06-13 16:26:15 +0300

    Create kokeilu.php

---
+++ Public/kokeilu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>DCSS</title>
+  </head>
+<body>
+
+<form action="koeyhteys.php" method="post">
+Lempielokuva: <input type="text" name="formelokuva" maxlenght="20" value="<?php=$elokuva;?>">
+<input type="submit" name="formsubmit" value="Lähetä">
+</form>
+</body>
+</html>
