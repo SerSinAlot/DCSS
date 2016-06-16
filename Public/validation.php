a4c284c7f81dc5f0dd6281cafc8dcb65707bc6cc
SerSinAlot <aarni.korhonen@aol.com>
2016-06-16 10:22:49 +0300

    muokattu validation

--- Public/validation.php
+++ Public/validation.php
@@ -13,35 +13,21 @@ function validateFields ($fields, $rules)
 		// continue evaluating the rest of the line. Keep repeating this while the line begins with an 
 		// if-condition. If it fails any of the conditions, don't bother validating the rest of the line
 		$satisfies_if_conditions = true;
-		while (preg_match("/^if:/", "", $row[0])
-		
+		while (preg_match("/^if:/", $row[0])
+		{
+		$condition = preg_replace("/^if:/", "", $row[0]);
 		// check if it's a = or != test
-		$comparison = "equal";
-		$parts = array(;
-		if (preg_match("/!=!", $condition))
+		$parts = array();
+		if (preg_match("/!=/", $condition))
 		{
 			$parts = explode("!=", $condition);
-			$comparison = "not_equal";
 		}
 		else
 			$parts = explode("=", $condition);
 		
 		$field_to_check = $parts[0];
 		$value_to_check = $parts[1];
-		
-		// if the VALUE is NOT the same, we don't need to validate this field. Return.
-		if ($comparison == "equal" && $fields[$field_to_check] != $value_to_check)
-		{
-			$satisfies_if_conditions = false;
-			break;
 		}
-		else if ($comparison == "not equal" && $fields[$field_to_check] == $value_to_check)
-		{
-			$satisfies_if_conditions = false;
-			break;
-		}
-		else
-			array_shift($row);	// remove this if-condition from line, and continue validating line
 	}
 	
 	if (!$satisfies_if_conditions)
@@ -53,24 +39,24 @@ function validateFields ($fields, $rules)
 	// depending on validation test, store strings for later use
 	if (count($row) == 6)
 	{
-      $field_name2   = $row[2];
-      $field_name3   = $row[3];
-      $date_flag     = $row[4];
-      $error_message = $row[5];
-    }
-    else if (count($row) == 5)     // reg_exp (WITH flags like g, i, m)
-    {
-      $field_name2   = $row[2];
-      $field_name3   = $row[3];
-      $error_message = $row[4];
-    }
-    else if (count($row) == 4)     // same_as, custom_alpha, reg_exp (without flags like g, i, m)
-    {
-      $field_name2   = $row[2];
-      $error_message = $row[3];
-    }
-    else
-      $error_message = $row[2];    // everything else!
+     	$field_name2   = $row[2];
+	$field_name3   = $row[3];
+     	$field_name4   = $row[4];
+     	$error_message = $row[5];
+	}
+    	else if (count($row) == 5)     // reg_exp (WITH flags like g, i, m)
+    	{
+      	$field_name2   = $row[2];
+      	$field_name3   = $row[3];
+      	$error_message = $row[4];
+    	}
+    	else if (count($row) == 4)     // same_as, custom_alpha, reg_exp (without flags like g, i, m)
+    	{
+      	$field_name2   = $row[2];
+      	$error_message = $row[3];
+    	}
+    	else
+      	$error_message = $row[2];    // everything else!
 
 
     // if the requirement is "length=...", rename requirement to "length" for switch statement
@@ -108,132 +94,9 @@ function validateFields ($fields, $rules)
 
       // doesn't fail if field is empty
       case "valid_email":
-				$regexp="/^[a-z0-9]+([_+\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i";    
+		$regexp="/^[a-z0-9]+([_+\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i";    
         if (isset($fields[$field_name]) && !empty($fields[$field_name]) && !preg_match($regexp, $fields[$field_name]))
           $errors[] = $error_message;
-        break;
-
-      case "length":
-        $comparison_rule = "";
-        $rule_string     = "";
-
-        if      (preg_match("/length=/", $length_requirements))
-        {
-          $comparison_rule = "equal";
-          $rule_string = preg_replace("/length=/", "", $length_requirements);
-        }
-        else if (preg_match("/length>=/", $length_requirements))
-        {
-          $comparison_rule = "greater_than_or_equal";
-          $rule_string = preg_replace("/length>=/", "", $length_requirements);
-        }
-        else if (preg_match("/length<=/", $length_requirements))
-        {
-          $comparison_rule = "less_than_or_equal";
-          $rule_string = preg_replace("/length<=/", "", $length_requirements);
-        }
-        else if (preg_match("/length>/", $length_requirements))
-        {
-          $comparison_rule = "greater_than";
-          $rule_string = preg_replace("/length>/", "", $length_requirements);
-        }
-        else if (preg_match("/length</", $length_requirements))
-        {
-          $comparison_rule = "less_than";
-          $rule_string = preg_replace("/length</", "", $length_requirements);
-        }
-
-        switch ($comparison_rule)
-        {
-          case "greater_than_or_equal":
-            if (!(strlen($fields[$field_name]) >= $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "less_than_or_equal":
-            if (!(strlen($fields[$field_name]) <= $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "greater_than":
-            if (!(strlen($fields[$field_name]) > $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "less_than":
-            if (!(strlen($fields[$field_name]) < $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "equal":
-            // if the user supplied two length fields, make sure the field is within that range
-            if (preg_match("/-/", $rule_string))
-            {
-              list($start, $end) = explode("-", $rule_string);
-              if (strlen($fields[$field_name]) < $start || strlen($fields[$field_name]) > $end)
-                $errors[] = $error_message;
-            }
-            // otherwise, check it's EXACTLY the size the user specified 
-            else
-            {
-              if (strlen($fields[$field_name]) != $rule_string)
-                $errors[] = $error_message;
-            }     
-            break;       
-        }
-        break;
-
-      case "range":
-        $comparison_rule = "";
-        $rule_string     = "";
-
-        if      (preg_match("/range=/", $range_requirements))
-        {
-          $comparison_rule = "equal";
-          $rule_string = preg_replace("/range=/", "", $range_requirements);
-        }
-        else if (preg_match("/range>=/", $range_requirements))
-        {
-          $comparison_rule = "greater_than_or_equal";
-          $rule_string = preg_replace("/range>=/", "", $range_requirements);
-        }
-        else if (preg_match("/range<=/", $range_requirements))
-        {
-          $comparison_rule = "less_than_or_equal";
-          $rule_string = preg_replace("/range<=/", "", $range_requirements);
-        }
-        else if (preg_match("/range>/", $range_requirements))
-        {
-          $comparison_rule = "greater_than";
-          $rule_string = preg_replace("/range>/", "", $range_requirements);
-        }
-        else if (preg_match("/range</", $range_requirements))
-        {
-          $comparison_rule = "less_than";
-          $rule_string = preg_replace("/range</", "", $range_requirements);
-        }
-        
-        switch ($comparison_rule)
-        {
-          case "greater_than":
-            if (!($fields[$field_name] > $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "less_than":
-            if (!($fields[$field_name] < $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "greater_than_or_equal":
-            if (!($fields[$field_name] >= $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "less_than_or_equal":
-            if (!($fields[$field_name] <= $rule_string))
-              $errors[] = $error_message;
-            break;
-          case "equal":
-            list($start, $end) = explode("-", $rule_string);
-
-            if (($fields[$field_name] < $start) || ($fields[$field_name] > $end))
-              $errors[] = $error_message;
-            break;
-        }
         break;
         
       case "same_as":
