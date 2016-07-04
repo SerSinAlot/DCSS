582ec1f0b65935b688855485db6eac2208691ce1
SerSinAlot <aarni.korhonen@aol.com>
2016-07-04 09:45:55 +0300

    Poistettu ylimääräiset

--- Public/dcss.php
+++ Public/dcss.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $errors = array();
 $fields = array();
 $success_message = "";
@@ -24,7 +23,7 @@ if (isset($_POST['submit']))
 	$rules[] = "required,otsikko,Anna viestillesi otsikko.";
 	$rules[] = "letters_digits_only,otsikko,Vain kirjaimia ja numeroita otsikossa.";
 	$rules[] = "required,viesti,Kerro meille huolesi viestikenttään.";
-	
+	$rules[] = "textbox,viesti,Vain kirjaimia ja numeroita sekä sallittuja erikoismerkkejä viestikenttään.";	
 	$rules[] = "required,tag,Valitse pyyntöösi sopiva tag.";
 	
 	$errors = validateFields($_POST, $rules);
@@ -39,16 +38,49 @@ if (isset($_POST['submit']))
 	else
 	{
 		$message = "Tiedot syötetty onnistuneesti";
-		$_SESSION['etunimi']=$_POST['etunimi'];
-		$_SESSION['sukunimi']=$_POST['sukunimi'];
-		$_SESSION['luokka']=$_POST['luokka'];
-		$_SESSION['email']=$_POST['email'];
-		$_SESSION['puhelin']=$_POST['puhelin'];
-		$_SESSION['otsikko']=$_POST['otsikko'];
-		$_SESSION['viesti']=$_POST['viesti'];
-		$_SESSION['tag']=$_POST['tag'];
-		header("Location: yhteys2.php");
-	}
+		
+$servername="192.168.206.159";
+$username="webuser";
+$password="Passw0rd";
+$dbname="DCSS";
+
+/*Muodosta yhteys*/
+$yhteys = mysqli_connect($servername, $username, $password, $dbname);
+
+/*Testaa yhteys*/
+if (!$yhteys) {
+	die("Yhteys epäonnistui: " . mysqli_connect_error());
+}
+echo "Yhteys onnistui. ";
+
+$etunimi=mysqli_real_escape_string($yhteys, $_POST['etunimi']);
+$sukunimi=mysqli_real_escape_string($yhteys, $_POST['sukunimi']);
+$luokka=mysqli_real_escape_string($yhteys, $_POST['luokka']);
+$email=mysqli_real_escape_string($yhteys, $_POST['email']);
+$puhelin=mysqli_real_escape_string($yhteys, $_POST['puhelin']);
+$otsikko=mysqli_real_escape_string($yhteys, $_POST['otsikko']);
+$viesti=mysqli_real_escape_string($yhteys, $_POST['viesti']);
+$tag=mysqli_real_escape_string($yhteys, $_POST['tag']);
+
+$sql="INSERT INTO `Tiketit`(`Etunimi`, `Sukunimi`, `Luokka`, `Email`, `PuhNro`, `Otsikko`, `Viesti`, `Tag`)
+VALUES ('$etunimi', '$sukunimi', '$luokka', '$email', '$puhelin', '$otsikko', '$viesti', '$tag')";
+
+if(mysqli_query($yhteys, $sql)) 
+{
+	$success = "Tiedot tallennettu onnistuneesti.";
+	echo $success;
+} 
+else 
+{
+	echo "Virhe: sinun sähköpostiosoitteellasi on olemassa avoin tiketti.";
+	$fields = $_POST;
+}
+
+mysqli_close($yhteys);
+}
+$subject = 'Kiitos yhteydenotosta!';
+$message = 'Osaava ja innokas yhteisömme ottaa tehtävän hoidettavakseen, kunhan kiireiltään ehtii.';
+//mail($email, $subject, $message);
 }
 
 if(!isset($fields["etunimi"])) $fields["etunimi"] = "";
@@ -60,12 +92,20 @@ if(!isset($fields["otsikko"])) $fields["otsikko"] = "";
 if(!isset($fields["viesti"])) $fields["viesti"] = "";
 if(!isset($fields["tag"])) $fields["tag"] = "";
 
+function testi3($data)
+{
+	$data=trim($data);
+	$data=stripcslashes($data);
+	$data=htmlspecialchars($data);
+	return $data;
+}
 ?>
 
 <!DOCTYPE HTML>
 <html>
   <head>
-  <link rel="stylesheet" type="text/css" href="style.css">
+   <meta content="text/html; charset=utf-8"></meta>
+   <link rel="stylesheet" type="text/css" href="style.css">
     <title>DCSS</title>
   </head>
 <body>
@@ -74,7 +114,7 @@ if(!isset($fields["tag"])) $fields["tag"] = "";
 <tr>
   <td>
 
-    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post" accept-charset="utf-8">
 
     <p class="title"><font size="20">Data Center Support System</font></p>
     <p>Hei, kun metsään huutaa, niin DC vastaa.</p>
@@ -97,7 +137,6 @@ if (!empty($message))
 {
 	echo "<div class='notify'>$success_message</div>";
 }
-
 ?>
 
 	<table class="demotable">
@@ -106,7 +145,7 @@ if (!empty($message))
 	  <td><input type="text" name="etunimi" value="<?=$fields['etunimi']?>" placeholder="Etunimi" /></td>
 	</tr>
 	<tr>
-	<td>Sukunimi:<span class="error"> *</span></td>
+	<td>Sukunimi:<span class="error"> *</span>&nbsp;&nbsp;</td>
 	  <td><input type="text" name="sukunimi" value="<?=$fields['sukunimi']?>" placeholder="Sukunimi" /></td>
 	</tr>
 	<tr>
@@ -127,7 +166,7 @@ if (!empty($message))
 	</tr>
 	<tr>
 	<td>Viesti:<span class="error"> *</span></td>
-	  <td><textarea name="viesti" rows="5" cols="40" maxlength="200" placeholder="Lyhyt kuvaus tehtävästä (max 200 merkkiä)" /><?php if(isset($_POST['viesti'])) {echo $_POST['viesti'];} ?></textarea></td>
+	  <td><textarea name="viesti" rows="5" cols="40" maxlength="200" placeholder="Lyhyt kuvaus tehtävästä (max 200 merkkiä). Sallittuja erikoismerkkejä -.,!?€&*+." /><?php if(isset($_POST['viesti'])) {if(!isset($success)){echo $_POST['viesti'];}} ?></textarea></td>
 	</tr>
 	<tr>
 	<td>Tag:<span class="error"> *</span></td>
